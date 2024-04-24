fad7f440d3403f52d06cd9fae3f74f2beb2d9ff3
nathanhalgumacoisa <nathan.henrique@aluno.senai.br>
2024-04-24 11:36:30 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const { Pool } = require('pg');
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+
+
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}🧙🪄✨🏳️‍⚧️`);
+  });
